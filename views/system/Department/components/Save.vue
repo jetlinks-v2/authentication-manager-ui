e95eb72bc39db35a0a661b63eb05353f618ccbe8
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-08-11 11:33:55 +0800

    fix: 【数据字典】数据字典左侧进行删除时，提示错误[35823] (#15)
    * fix: 【数据字典】数据字典左侧进行删除时，提示错误[35823]

--- views/system/Department/components/Save.vue
+++ views/system/Department/components/Save.vue
@@ -98,9 +98,9 @@ const confirm = () => {
   formRef.value
     ?.validate()
     .then((_data: any) => {
-      // if(!_data.parentId){
-      //   _data.parentId = undefined
-      // }
+      if(!_data.parentId){
+        _data.parentId = ""
+      }
       run(props.data.id ? { ...props.data, ..._data } : _data)
     })
 }
