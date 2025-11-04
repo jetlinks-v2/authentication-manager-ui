311adaa238d1d5248a788f3338bd41820183ef15
XieYongHong <18010623010@163.com>
2025-11-04 14:56:11 +0800

    fix: 修复错误使用模块路径

--- register.ts
+++ register.ts
@@ -1,4 +1,9 @@
+import { getTreeData_api } from '@authentication-manager-ui/api/system/department'
+
 export default {
+  apis: {
+    getTreeData_api
+  },
   components: {
     Calendar: defineAsyncComponent(() => import("./views/system/Calendar/FullCalendar/index.vue")),
   }
